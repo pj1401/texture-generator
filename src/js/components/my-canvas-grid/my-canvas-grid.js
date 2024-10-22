3f51c63c27230b93cfebf28069702190d3b2cdc6
Patricia Johansson <pj222uc@student.lnu.se>
2024-10-22 09:12:13 +0200

    Add generate Perlin noise method

--- src/js/components/my-canvas-grid/my-canvas-grid.js
+++ src/js/components/my-canvas-grid/my-canvas-grid.js
@@ -4,6 +4,8 @@
  * @version 1.0.0
  */
 
+// import PerlinNoise from './lib/perlin-noise/index.js'
+
 import { htmlTemplate } from './my-canvas-grid.html.js'
 
 customElements.define('my-canvas-grid',
@@ -11,6 +13,11 @@ customElements.define('my-canvas-grid',
    * Represents a my-canvas-grid element.
    */
   class extends HTMLElement {
+    #ctx
+    #width
+    #height
+    #scale
+
     /**
      * Creates an instance of the current type.
      */
@@ -25,9 +32,41 @@ customElements.define('my-canvas-grid',
       // TODO: A method that generates a noise value for every pixel in the grid.
       // TODO: The noise value has a corresponding colour.
 
-      const canvas = this.shadowRoot.querySelector('#canvas-grid').getContext('2d')
-      canvas.fillStyle = 'green'
-      canvas.fillRect(10, 10, 380, 380)
+      const canvas = this.shadowRoot.querySelector('#canvas-grid')
+      this.#ctx = canvas.getContext('2d')
+
+      this.#width = canvas.width
+      this.#height = canvas.height
+      this.#scale = 0.05
+
+      this.generatePerlinNoise()
     }
+
+    /**
+     * Watches the attributes for changes on the element.
+     *
+     * @returns {string[]} A string array of the attributes.
+     */
+    static get observedAttributes () {
+      return ['width', 'height', 'seed']
+    }
+
+    /**
+     * Generates the image.
+     */
+    generatePerlinNoise () {
+      // TODO: Step through each pixel and generate a colour.
+
+      const noise = this.getNoiseValue()
+      const color = this.getColorValue(noise)
+      this.fillPixel(color)
+    }
+
+    /**
+     * Colours a pixel on the grid.
+     *
+     * @param {number} colorValue - The colour value in rgb.
+     */
+    fillPixel (colorValue) {}
   }
 )
