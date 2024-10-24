ae7ce890348d94e8f96dbfce58474d9f7accd153
Patricia Johansson <pj222uc@student.lnu.se>
2024-10-24 12:10:17 +0200

    Add tests for NoiseGrid

--- test/NoiseGrid.test.js
+++ test/NoiseGrid.test.js
@@ -7,6 +7,36 @@
 
 import NoiseGrid from '../src/js/components/my-canvas-grid/lib/NoiseGrid.js'
 
+describe('Initialising objects', () => {
+  test('New NoiseGrid', () => {
+    const noiseGrid = new NoiseGrid(400, 400, 1)
+
+    expect({
+      width: noiseGrid.width,
+      height: noiseGrid.height,
+      seed: noiseGrid.seed
+    }).toEqual({
+      width: 400,
+      height: 400,
+      seed: 1
+    })
+  })
+
+  test('New NoiseGrid with undefined seed', () => {
+    const noiseGrid = new NoiseGrid(400, 400, undefined)
+
+    expect({
+      width: noiseGrid.width,
+      height: noiseGrid.height,
+      seed: noiseGrid.seed
+    }).toEqual({
+      width: 400,
+      height: 400,
+      seed: 0
+    })
+  })
+})
+
 describe('Using non numbers', () => {
   test('New grid with NaN as seed', () => {
     expect(() => new NoiseGrid(400, 400, NaN)).toThrow(TypeError)
