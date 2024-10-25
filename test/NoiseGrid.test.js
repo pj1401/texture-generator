38dbdba78936c74fcf7554544c0fd2b45223563f
Patricia Johansson <pj222uc@student.lnu.se>
2024-10-25 15:36:56 +0200

    Restructure the NoiseGrid tests

--- test/NoiseGrid.test.js
+++ test/NoiseGrid.test.js
@@ -7,33 +7,52 @@
 
 import NoiseGrid from '../src/js/components/my-canvas-grid/lib/NoiseGrid.js'
 
-describe('Initialising objects', () => {
-  test('New NoiseGrid', () => {
-    const noiseGrid = new NoiseGrid(400, 400, 1)
+/**
+ * Represents a test case for NoiseGrid.
+ */
+class NoiseGridTestCase {
+  /**
+   * Initialises the object.
+   *
+   * @param {NoiseGrid} noiseGrid - The grid object to test.
+   * @param {object} expected - The expected values
+   */
+  constructor (noiseGrid, expected) {
+    this.noiseGrid = noiseGrid
+    this.expected = expected
+  }
 
+  /**
+   * Run the test case.
+   */
+  runTest () {
     expect({
-      width: noiseGrid.width,
-      height: noiseGrid.height,
-      seed: noiseGrid.seed
-    }).toEqual({
+      width: this.noiseGrid.width,
+      height: this.noiseGrid.height,
+      seed: this.noiseGrid.seed
+    }).toEqual(this.expected)
+  }
+}
+
+describe('Initialising NoiseGrid objects', () => {
+  test('New NoiseGrid', () => {
+    const testCase = new NoiseGridTestCase(new NoiseGrid(400, 400, 1), {
       width: 400,
       height: 400,
       seed: 1
     })
+
+    testCase.runTest()
   })
 
   test('New NoiseGrid with undefined seed', () => {
-    const noiseGrid = new NoiseGrid(400, 400, undefined)
-
-    expect({
-      width: noiseGrid.width,
-      height: noiseGrid.height,
-      seed: noiseGrid.seed
-    }).toEqual({
+    const testCase = new NoiseGridTestCase(new NoiseGrid(400, 400, undefined), {
       width: 400,
       height: 400,
       seed: 0
     })
+
+    testCase.runTest()
   })
 })
 
